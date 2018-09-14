bc8c7fc008edf1ead55db52246e358bb84c8359e
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-14 18:36:01 -0400

    Create warning.sh

---
+++ warning.sh
@@ -0,0 +1,9 @@
+message_box "Ultimate Crypto-Server Setup Installer" \
+"You have choosen to install YiiMP Multi Server!
+\n\nThis option will install YiiMP across multiple servers.
+\n\nThis option is for more advanced pool operators and has the following requirements:
+\n\n1.You must log in to each server and create a user account. If no user account exists, run this script to have one created.
+\n\n2.Your servers must have a private IP or if your provider doesn't offer private IP's then you must run the Wireguard installer on each server.
+\n\n3.After the user accounts are created and if needed Wireguard is installed, only then can you setup YiiMP multi-server.
+\n\n4.To begin installation of YiiMP the installer MUST be ran from the server that will host your DataBase.
+\n\nNOTE: Failing to follow the above directions will result in a failed installation!"
