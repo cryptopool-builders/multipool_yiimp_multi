2042af92d4d7fe521bd487f952510f33f593eb10
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-15 18:23:40 -0400

    Update nginx_upgrade.sh

--- nginx_upgrade.sh
+++ nginx_upgrade.sh
@@ -12,11 +12,11 @@ echo 'deb https://nginx.org/packages/mainline/ubuntu/ xenial nginx
 deb-src https://nginx.org/packages/mainline/ubuntu/ xenial nginx
 ' | sudo -E tee /etc/apt/sources.list.d/nginx.list >/dev/null 2>&1
 
-hide_output sudo apt-get update
-apt_install nginx
+hide_output sudo apt-get update;
+apt_install nginx;
 
 sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
 sudo cp -r /tmp/nginx.conf /etc/nginx/
 
-restart_service nginx
-restart_service php7.2-fpm
+restart_service nginx;
+restart_service php7.2-fpm;
