969f5d4cd76a76bd1ae6c0396538cb649d4ea6f9
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-15 19:52:34 -0400

    Update nginx_upgrade.sh

--- nginx_upgrade.sh
+++ nginx_upgrade.sh
@@ -6,17 +6,22 @@ source $STORAGE_ROOT/yiimp/.yiimp.conf
 # NGINX upgrade
 echo Upgrading NGINX...
 hide_output sudo wget https://nginx.org/keys/nginx_signing.key
+wait $!
 hide_output sudo apt-key add nginx_signing.key
+wait $!
 sudo rm -r nginx_signing.key
 echo 'deb https://nginx.org/packages/mainline/ubuntu/ xenial nginx
 deb-src https://nginx.org/packages/mainline/ubuntu/ xenial nginx
 ' | sudo -E tee /etc/apt/sources.list.d/nginx.list >/dev/null 2>&1
 
 hide_output sudo apt-get update;
+wait $!
 apt_install nginx;
+wait $!
 
 sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
 sudo cp -r /tmp/nginx.conf /etc/nginx/
 
 restart_service nginx;
+wait $!
 restart_service php7.2-fpm;
