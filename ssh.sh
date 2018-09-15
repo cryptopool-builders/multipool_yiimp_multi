60dcbe48204d01dac3324d95c83019339188fd8b
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-14 21:14:32 -0400

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,25 @@
+if [ -z "$DISABLE_FIREWALL" ]; then
+# Install `ufw` which provides a simple firewall configuration.
+echo Installing UFW...
+apt_install ufw
+
+# Allow incoming connections.
+ufw_allow ssh;
+ufw_allow http;
+ufw_allow https;
+
+# ssh might be running on an alternate port. Use sshd -T to dump sshd's #NODOC
+# settings, find the port it is supposedly running on, and open that port #NODOC
+# too. #NODOC
+SSH_PORT=$(sshd -T 2>/dev/null | grep "^port " | sed "s/port //") #NODOC
+if [ ! -z "$SSH_PORT" ]; then
+if [ "$SSH_PORT" != "22" ]; then
+
+echo Opening alternate SSH port $SSH_PORT. #NODOC
+ufw_allow $SSH_PORT #NODOC
+
+fi
+fi
+
+sudo ufw --force enable;
+fi #NODOC
