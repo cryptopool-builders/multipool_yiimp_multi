18c9ccdef9960460d862a73b332927f29e3ed465
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-14 20:43:31 -0400

    Create system_single_db.sh

---
+++ system_single_db.sh
@@ -0,0 +1,102 @@
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by cryptopool.builders for crypto use...
+
+clear
+source /etc/functions.sh
+source $STORAGE_ROOT/yiimp/.yiimp.conf
+
+# Check swap
+echo Checking if swap space is needed and if so creating...
+SWAP_MOUNTED=$(cat /proc/swaps | tail -n+2)
+SWAP_IN_FSTAB=$(grep "swap" /etc/fstab)
+ROOT_IS_BTRFS=$(grep "\/ .*btrfs" /proc/mounts)
+TOTAL_PHYSICAL_MEM=$(head -n 1 /proc/meminfo | awk '{print $2}')
+AVAILABLE_DISK_SPACE=$(df / --output=avail | tail -n 1)
+if
+[ -z "$SWAP_MOUNTED" ] &&
+[ -z "$SWAP_IN_FSTAB" ] &&
+[ ! -e /swapfile ] &&
+[ -z "$ROOT_IS_BTRFS" ] &&
+[ $TOTAL_PHYSICAL_MEM -lt 1900000 ] &&
+[ $AVAILABLE_DISK_SPACE -gt 5242880 ]
+then
+echo "Adding a swap file to the system..."
+
+# Allocate and activate the swap file. Allocate in 1KB chuncks
+# doing it in one go, could fail on low memory systems
+dd if=/dev/zero of=/swapfile bs=1024 count=$[1024*1024] status=none
+if [ -e /swapfile ]; then
+chmod 600 /swapfile
+hide_output mkswap /swapfile
+swapon /swapfile
+fi
+
+# Check if swap is mounted then activate on boot
+if swapon -s | grep -q "\/swapfile"; then
+echo "/swapfile  none swap sw 0  0" >> /etc/fstab
+else
+echo "ERROR: Swap allocation failed"
+fi
+fi
+
+# Set timezone
+echo Setting TimeZone to UTC...
+if [ ! -f /etc/timezone ]; then
+echo "Setting timezone to UTC."
+echo "Etc/UTC" > sudo /etc/timezone
+restart_service rsyslog
+fi
+
+# Add repository
+echo Adding the required repsoitories...
+if [ ! -f /usr/bin/add-apt-repository ]; then
+echo "Installing add-apt-repository..."
+hide_output sudo apt-get -y update
+apt_install software-properties-common
+fi
+# MariaDB
+echo Installing MariaDB Repository...
+hide_output sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.evowise.com/mariadb/repo/10.3/ubuntu xenial main'
+
+# Upgrade System Files
+echo Updating system packages...
+hide_output sudo apt-get update
+echo Upgrading system packages...
+if [ ! -f /boot/grub/menu.lst ]; then
+apt_get_quiet upgrade
+else
+sudo rm /boot/grub/menu.lst
+hide_output sudo update-grub-legacy-ec2 -y
+apt_get_quiet upgrade
+fi
+echo Running Dist-Upgrade...
+apt_get_quiet dist-upgrade
+echo Running Autoremove...
+apt_get_quiet autoremove
+
+echo Installing Base system packages...
+apt_install python3 python3-dev python3-pip \
+wget curl git sudo coreutils bc \
+haveged pollinate unzip ntpdate \
+unattended-upgrades cron ntp fail2ban screen
+
+# ### Seed /dev/urandom
+echo Initializing system random number generator...
+hide_output dd if=/dev/random of=/dev/urandom bs=1 count=32 2> /dev/null
+hide_output sudo pollinate -q -r
+
+echo Installing YiiMP Required system packages...
+if [ -f /usr/sbin/apache2 ]; then
+echo Removing apache...
+hide_output apt-get -y purge apache2 apache2-*
+hide_output apt-get -y --purge autoremove
+fi
+hide_output sudo apt-get update
+
+apt_install mariadb-server mariadb-client
+
+echo Downloading selected YiiMP Repo...
+hide_output sudo git clone $YiiMPRepo $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+
+cd $HOME/multipool/yiimp_single
