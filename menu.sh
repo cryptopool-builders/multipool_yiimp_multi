94ebcc107d428ed8e1c8c1908ba5fdd6a2f80fc5
cryptopool <cryptopool.builders@gmail.com>
2018-11-01 21:02:17 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -7,8 +7,8 @@ source /etc/functions.sh
 
 RESULT=$(dialog --stdout --title "Ultimate Crypto-Server Setup Installer" --menu "Choose one" -1 60 4 \
 1 "Install Wireguard all servers" \
-2 "YiiMP DB-Stratum, Web, Daemon" \
-3 "YiiMP DB, Web, Stratum, Daemon" \
+2 "YiiMP, DB-Stratum, Web, Daemon" \
+3 "YiiMP, DB, Web, Stratum, Daemon" \
 4 Exit)
 if [ $RESULT = ]
 then
