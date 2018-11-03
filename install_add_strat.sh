067dcea8b4fff91965b65ce1f3495a0f1eedc0ff
cryptopool <cryptopool.builders@gmail.com>
2018-11-02 23:21:41 -0400

    update

--- install_add_strat.sh
+++ install_add_strat.sh
@@ -7,11 +7,5 @@ cd $HOME/multipool/yiimp_multi
 
 # Begin Installation
 source questions_add_strat.sh
-source setsid_add_stratum_server.sh
 source add_strat_db.sh
-
-cd ~
-clear
-echo Installation of your YiiMP additional stratum server is now completed.
-echo You *MUST* reboot this machine to finalize the system updates and folder permissions! YiiMP will not function until a reboot is performed!
-echo
+source setsid_add_stratum_server.sh
