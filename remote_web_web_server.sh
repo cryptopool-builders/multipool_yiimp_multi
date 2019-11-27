a7d507c823bec02df3f39a15073bccc54a391104
cryptopool <cryptopool.builders@gmail.com>
2019-11-26 20:55:35 -0500

    multi-server updates
    multi-server updates

--- remote_web_web_server.sh
+++ remote_web_web_server.sh
@@ -29,6 +29,8 @@ sudo sed -i "s|ROOTDIR=/data/yiimp|ROOTDIR=${STORAGE_ROOT}/yiimp/site|g" /bin/yi
 echo -e "$GREEN Done...$COL_RESET"
 
 echo -e " Creating nginx web configuration files...$COL_RESET"
+sudo chmod u+x /tmp/nginx_domain_nonssl.sh;
+source /tmp/nginx_domain_nonssl.sh;
 if [[ ("$UsingSubDomain" == "y" || "$UsingSubDomain" == "Y" || "$UsingSubDomain" == "yes" || "$UsingSubDomain" == "Yes" || "$UsingSubDomain" == "YES") ]]; then
   sudo chmod u+x /tmp/nginx_subdomain_nonssl.sh;
   source /tmp/nginx_subdomain_nonssl.sh;
@@ -40,9 +42,6 @@ if [[ ("$UsingSubDomain" == "y" || "$UsingSubDomain" == "Y" || "$UsingSubDomain"
     if [[ ("$InstallSSL" == "y" || "$InstallSSL" == "Y" || "$InstallSSL" == "yes" || "$InstallSSL" == "Yes" || "$InstallSSL" == "YES") ]]; then
       sudo chmod u+x /tmp/nginx_domain_ssl.sh;
       source /tmp/nginx_domain_ssl.sh;
-    else
-      sudo chmod u+x /tmp/nginx_domain_nonssl.sh;
-      source /tmp/nginx_domain_nonssl.sh;
     fi
 fi
 echo -e "$GREEN Done...$COL_RESET"
