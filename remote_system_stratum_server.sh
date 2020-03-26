ff6e15eed74262cae4a9a0ac99e648962f1c2332
cryptopool <cryptopool.builders@gmail.com>
2020-03-26 13:23:18 -0400

    removed ubuntu 18.x package from ubuntu 16.x install list
    removed ubuntu 18.x package from ubuntu 16.x install list

--- remote_system_stratum_server.sh
+++ remote_system_stratum_server.sh
@@ -100,7 +100,7 @@ if [[ ("$DISTRO" == "16") ]]; then
 apt_install libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev libkrb5-dev \
 libldap2-dev libidn11-dev gnutls-dev librtmp-dev build-essential libtool  \
 autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils \
-git pwgen mariadb-client fail2ban gnupg2 ca-certificates lsb-release libsodium-dev default-libmysqlclient-dev \
+git pwgen mariadb-client fail2ban gnupg2 ca-certificates lsb-release libsodium-dev \
 libnghttp2-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev libpsl-dev libkrb5-dev;
 wait $!
 else
